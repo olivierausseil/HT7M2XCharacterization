cc8bd9c24baafff646b4c360d7d3c7ae5d1f9ed6
olivierausseil <olivier.ausseil@sensing-labs.com>
2017-04-11 07:10:51 +0000

    initial adding

---
+++ CharacterizationTest.py
@@ -0,0 +1,212 @@
+#!/usr/bin/python2
+
+import time
+import LoRaBench
+import numpy as np
+import argparse
+import RPi.GPIO as GPIO
+import logging
+
+#GPIO initialization
+ledPin = 27
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(ledPin, GPIO.IN)
+
+#recover the date and hour, choose the format to acquisition
+#FORMAT = '%(asctime)s %(message)s',datefmt='%a %d %b %Y %H:%M:%S'
+logging.basicConfig(level = logging.INFO,
+                    format= '%(asctime)s %(message)s',
+                    datefmt='%a %d %b %Y %H:%M:%S')
+logging.Formatter.converter = time.gmtime
+
+# variable initialization
+maxword = 255
+maxbyte = 65535
+flagTime = 0
+timeDetection = 0
+endTimeDetection = 0
+
+# creation of PARSER : do argument when i launch the script
+parser = argparse.ArgumentParser()
+parser.add_argument('-SF', action='store', dest='spreadingFactor')
+parser.add_argument('-PP', action='store', dest='power')
+parser.add_argument('-FF', action='store', dest='frequencyOffset')
+parser.add_argument('-NN', action='store', dest='numberOfPackets')
+parser.add_argument('-LL', action='store', dest='lenghtOfPackets')
+parser.add_argument('-DD', action='store', dest='delay')
+results = parser.parse_args()
+
+SendFrames = [0x04] #identification to SendFrames LoraBench datasheet
+
+# SPREADING FACTOR -----------------------
+if None == results.spreadingFactor:
+    spreadingFactor = int(12) #default value
+else:
+    spreadingFactor = int(results.spreadingFactor,16)
+
+# manage argument limits (0-255) (see datasheet_LoRaBench_160617_A05_FirmwareSpecification_EN __ p6 __ Command SendFrames)
+if spreadingFactor < 0:
+    spreadingFactor = 0
+
+if spreadingFactor > maxword:
+    spreadingFactor = maxword
+SendFrames.append(spreadingFactor)
+
+# POWER ------------------------------------------
+if None == results.power:
+    power = int(14) #default value
+else:
+    power = int(results.power,16)
+
+# manage argument limits (0-255) (see datasheet_LoRaBench_160617_A05_FirmwareSpecification_EN __ p6 __ Command SendFrames)
+if power < 0:
+    power = 0
+
+if power > maxword:
+    power = maxword
+SendFrames.append(power)
+
+# FREQUENCY OFFSET -------------------------------------
+if None == results.frequencyOffset:
+    frequencyOffset = int(5100)
+else:
+    frequencyOffset = int(results.frequencyOffset,16)
+
+if frequencyOffset < 0:
+    frequencyOffset = 0
+
+if frequencyOffset > maxbyte:
+    frequencyOffset = maxbyte
+
+# cut in two the data
+SendFrames.append(frequencyOffset >> 8) # MSB
+SendFrames.append(frequencyOffset & 0xFF) # LSB
+
+
+# NUMBER OF PACKETS -------------------------------------
+if None == results.numberOfPackets:
+    numberOfPackets = int(1)
+else:
+    numberOfPackets = int(results.numberOfPackets,16)
+
+if numberOfPackets < 0:
+    numberOfPackets = 0
+
+if numberOfPackets > maxbyte:
+    numberOfPackets = maxbyte
+
+# cut in two the data
+SendFrames.append(numberOfPackets >> 8) # MSB
+SendFrames.append(numberOfPackets & 0xFF) # LSB
+
+# LENGHT OF PACKETS -------------------------------------
+if None == results.lenghtOfPackets:
+    lenghtOfPackets = int(10) #default value
+else:
+    lenghtOfPackets = int(results.lenghtOfPackets,16)
+
+# manage argument limits (0-255) (see datasheet_LoRaBench_160617_A05_FirmwareSpecification_EN __ p6 __ Command SendFrames)
+if lenghtOfPackets < 0:
+    lenghtOfPackets = 0
+
+if lenghtOfPackets > maxword:
+    lenghtOfPackets = maxword
+
+SendFrames.append(lenghtOfPackets)
+
+# DELAY --------------------------------------------------
+if None == results.delay:
+    delay = int(32) #default value
+else:
+    delay = int(results.delay,16)
+
+# manage argument limits (0-255) (see datasheet_LoRaBench_160617_A05_FirmwareSpecification_EN __ p6 __ Command SendFrames)
+if delay < 0:
+    delay = 0
+
+if delay > maxword:
+    delay = maxword
+
+SendFrames.append(delay)
+
+#print ( "Initial command frame: " + SendFrames)
+
+ # http://stackoverflow.com/questions/9448029/print-an-integer-array-as-hexadecimal-numbers
+np.set_printoptions(formatter={'int':lambda x:hex(int(x))})
+#np.set_printoptions(formatter={'int':hex})
+#np.set_printoptions(formatter={'int': '{: 02x }'.format})
+print ( "Initial command: " + str(np.array(SendFrames)))
+#we go encode and create my frame to send
+LoRaBench.LoRaBenchInit( )
+LoRaBench.LoRaBenchSendFrame(SendFrames)
+
+# after send frame, we wait an answer with LoRaBench (the start answer)
+rxbuffer = LoRaBench.LoRaBenchReceiveFrame()
+
+# analyse the start answer and if OK log date and time of start of transmission
+if ((rxbuffer[0] != 0x84 ) or (rxbuffer[1] != 0x00)):
+    print "error, the start answer is false "
+else:
+    print "start answer is ok"
+    #log
+
+#time.sleep(2)
+# we delete the buffer to 0 for the following
+rxbuffer = []
+
+# check detection to the PIR and log date and time detection
+detection = GPIO.input(ledPin)
+timeout = time.time()
+
+while (1):
+    #detectionTemporary = GPIO.input(ledPin)
+    time.sleep(0.001)
+    if detection == 0 :
+        if detection != GPIO.input(ledPin):
+            # change of state
+            detection = GPIO.input(ledPin)
+            timeDetection = time.time()
+            logging.info(' : DETECTION !!!!!!')
+
+    if detection != 0:
+        if detection != GPIO.input(ledPin) :
+            # change of state
+            detection = GPIO.input(ledPin)
+            endTimeDetection = time.time()
+            logging.info(' : End of detection')
+            flagTime = 1
+
+
+    if flagTime == 1 :
+        finalTime = endTimeDetection - timeDetection
+        print ("detection time : "+ "%.2f" % finalTime)
+        print ''
+        break
+
+
+
+    if (time.time() - timeout) > 5:
+        break
+
+rxbuffer = LoRaBench.LoRaBenchReceiveFrame()
+print rxbuffer
+
+#after send frame, we wait an answer with LoRaBench (the stop answer)
+#rxbuffer = LoRaBench.LoRaBenchReceiveFrame()
+
+# analyse the end answer and if OK log date and time of end of transmission
+if ((rxbuffer[0] != 0x80 ) or (rxbuffer[1] != 0x04)):
+    print ("error, the stop answer is false : " + str(rxbuffer))
+else:
+    print "stop answer is ok"
+
+
+#while (1):
+#    rxbuffer = LoRaBench.LoRaBenchReceiveFrame()
+#    if ( (rxbuffer[0] == 0x80) & (rxbuffer[1] == 0x04) ):
+#        break
+
+#if rxbuffer != None:
+    # display answer
+#else:
+    # display no reception error
---
+++ LoRaBench.py
@@ -0,0 +1,187 @@
+#!/usr/bin/python2
+
+import serial
+import crcmod
+import binascii
+import numpy as np
+
+STX = 0x02
+ETX = 0x03
+
+def LoRaBenchInit():
+    # initialization of serial
+    global ser
+    try:
+        ser = serial.Serial(
+            port='/dev/ttyUSB1',
+            baudrate = 9600,
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+            bytesize=serial.EIGHTBITS,
+            timeout=1
+            )
+    except:
+        ser = serial.Serial(
+            port='/dev/ttyUSB0',
+            baudrate = 9600,
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+            bytesize=serial.EIGHTBITS,
+            timeout=1
+            )
+
+
+def ComputeCrC (input_byte_array):
+
+    # http://crcmod.sourceforge.net/crcmod.html#crcmod.Crc
+    # for the polynomial, the value is for the LoRaBench documentation
+    # and equaly, why use 0x18408 and not 0x8408 (see http://stackoverflow.com/questions/24851027/how-to-calculate-ansi-crc16-polynomial-0x8005-in-python3)
+    crc16 = crcmod.mkCrcFun(0x11021,rev=True,initCrc=0, xorOut=0x0000)
+
+    #print ("initial array:" + str(input_byte_array))
+
+    # creating the string format expected by crc16 (http://stackoverflow.com/questions/19210414/byte-array-to-hex-string)
+    inputstr = binascii.hexlify(bytearray(input_byte_array))
+    #print inputstr
+
+    # Computing CRC16 (http://stackoverflow.com/questions/35205702/calculating-crc16-in-python)
+    crc = crc16(inputstr.decode("hex"))
+    #print crc
+    #print hex(crc)
+    return crc
+
+def LoRaBenchSendFrame(input_byte_array):
+
+    global ser
+
+    #insert len in first position
+    input_byte_array.insert(0, len(input_byte_array)+3 )
+    #print ("len and array:" + str(input_byte_array))
+
+
+    crc = ComputeCrC (input_byte_array)
+
+
+    input_byte_array.append(crc & 0xFF)
+    input_byte_array.append(crc >> 8)
+    #print input_byte_array
+
+    #STX
+    input_byte_array.insert(0, STX )
+    #ETX
+    input_byte_array.append(ETX)
+
+    np.set_printoptions(formatter={'int':lambda x:hex(int(x))})
+    print( "Protocol command: " + str(np.array(input_byte_array)))
+
+    #send a frame to LoRaBench
+    ser.write(input_byte_array)
+
+
+
+def LoRaBenchReceiveExpectedChar( expected_byte ):
+
+    # initial byte read
+    searchSTX = ser.read(1)
+
+    # check loop
+    while( len(searchSTX) != 0 ):
+
+        # a byte has been received --> check its value
+        received_byte = ord(searchSTX[0]) # byte to int conversion
+
+        # if the irght one --> True
+        if (expected_byte == received_byte):
+            #print "STX reception success"
+            return True
+        # if the wrong one --> Try again
+        else:
+            print ("===> STX reception attempt failed: " + str(received_byte))
+            searchSTX = ser.read(1)
+
+    # timeout --> False
+    #print "STX reception timeout"
+    return False
+
+
+
+def LoRaBenchReceiveChars( num_bytes ):
+
+    return ser.read(num_bytes)
+
+
+def LoRaBenchReceiveFrame( ):
+
+    global ser
+
+    rxbufferlen = 0
+    rxbuffer = []
+
+    # receive STX
+    if ( LoRaBenchReceiveExpectedChar( STX ) == False ):
+        print "No STX"
+        return []
+
+    # receive Length
+    rxbuffer = LoRaBenchReceiveChars( 1 )
+    if len(rxbuffer) == 0:
+        print ""
+        print ( "Empty frame reception" )
+        return []
+    else:
+        rxbufferlen = ord(rxbuffer[0]) # byte to int conversion
+        print ""
+        print ( "Answer Lenght is: " + str(rxbufferlen) )
+
+    # rxbuffer is theorically the length of actual data + 3 (length itself + 2 bytes CRC)
+    # however we already have received length, soi we should ask for one byte less
+    # but ETX is not comprised within this length, and we still must receive it
+    # the conclusion is that we can keep rxbufferlen as this for receiving the whole frame, ETX included
+    np.set_printoptions(formatter={'int':lambda x:hex(int(x))})
+    # receive frame
+    rxbuffer = LoRaBenchReceiveChars( rxbufferlen )
+
+    # convert to ascii to int    ( see : http://www.linuxnix.com/pfotd-python-ord-function-examples/)
+    rxbuffer=[ord(i) for i in rxbuffer]
+    print ""
+    print( "Received frame: " + str(rxbuffer))
+    print( "Received frame: " + str(np.array(rxbuffer)))
+
+    #for calculate the CRC, we need to add the length of frame
+    rxbuffer.insert(0, rxbufferlen )
+    # we calculate the lenght of rxbuffer again because the size has change
+    rxbufferlen = rxbufferlen + 1
+
+    # verify CRC16
+    #to calculate the CRC the ETX byte and the CRC bytes aren't used
+    rxbufferCRC = rxbuffer[0:-3]
+    #print rxbufferCRC
+
+    #function to calculate the CRC16
+    crcCalculate = ComputeCrC (rxbufferCRC)
+    #print ("the CRC value is : " + str(crcCalculate))
+    #print ("the rxbuffer is : " + str(rxbuffer))
+
+    temp = rxbuffer[rxbufferlen-3] * 256 + ( rxbuffer[rxbufferlen-2])
+    #print ("rxbuffer[rxbufferlen-3] : " + str(rxbuffer[rxbufferlen-3]))
+    #print ("rxbuffer[rxbufferlen-2] : " + str(rxbuffer[rxbufferlen-2]))
+    #print ("temp : " + str(temp))
+    hexcrc = hex (crcCalculate)
+    if crcCalculate != temp:
+        print (" Wrong CRC")
+        return []
+    else:
+        print ""
+        print ("Valid CRC : " + str(hexcrc))
+
+
+    # verify that last byte is ETX
+    if (rxbuffer[rxbufferlen-1] != ETX):
+        print ("===> ETX reception failed: " + str(rxbuffer[rxbufferlen-1]))
+        return []
+    #else :
+        #print ""
+        #print "ETX reception success"
+
+    return rxbuffer[1:-3]
+    #return rxbuffer[]
